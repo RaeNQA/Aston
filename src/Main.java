9114c12b108837f598772916cceeafafbc260bb5
RaeNQA <raenqa@gmail.com>
2024-06-14 14:53:00 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        task1.printThreeWords();
+        task2.checkSumSign();
+        task3.printColor();
+        task4.compareNumbers();
+        task5.main();
+        task6.main();
+        task7.main();
+        task8.main();
+        task9.main();
+        task10.main();
+        task11.main();
+        task12.main();
+        task13.main();
+        task14.main();
     }
 }
---
+++ src/task6.java
@@ -0,0 +1,14 @@
+public class task6 {
+    public static void main() {
+        int num = 10;
+        checkNum(num);
+    }
+
+    public static void checkNum(int num) {
+        if (num >= 0) {
+            System.out.println("Положительное число");
+        } else {
+            System.out.println("Отрицательное число");
+        }
+    }
+}
